0a4078ddd31906057133213136252cb214c295af
Niiixkz <niiixkz@gmail.com>
2025-06-26 16:52:11 +0800

    Change switch_profile.sh to more simple code

--- hyprland/config/switch_profile.sh
+++ hyprland/config/switch_profile.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
+asusctl profile -n
+
 current=$(asusctl profile -p | grep -oE 'Quiet|Balanced|Performance')
-
-profiles=("Quiet" "Balanced" "Performance")
-
-for i in "${!profiles[@]}"; do
-    if [[ "${profiles[$i]}" == "$current" ]]; then
-        next_index=$(((i + 1) % ${#profiles[@]}))
-        next_profile=${profiles[$next_index]}
-        break
-    fi
-done
-
-asusctl profile -P "$next_profile"
-
-notify-send "ASUS Profile Switched" "Now using: $next_profile"
+notify-send "ASUS Profile Switched" "Now using: $current"
