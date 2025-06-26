bcc9ebb6b949b4054a35c7f441b5c2eaf76a8e39
Niiixkz <niiixkz@gmail.com>
2025-06-26 17:29:40 +0800

    Add init arg to toggle_touchpad.sh, reboot will keep the state

--- hyprland/config/toggle_touchpad.sh
+++ hyprland/config/toggle_touchpad.sh
@@ -1,10 +1,16 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 DEVICE="elan1201:00-04f3:3098-touchpad"
 VAR="device[$DEVICE]:enabled"
 STATE_FILE="$HOME/.cache/touchpad_state"
 
 last="$(cat "$STATE_FILE" 2>/dev/null || echo "true")"
+
+if [ "$1" = "init" ]; then
+    echo "$last" >"$STATE_FILE"
+    hyprctl keyword "$VAR" "$last"
+    exit 0
+fi
 
 if [ "$last" = "true" ]; then
     hyprctl keyword "$VAR" false
