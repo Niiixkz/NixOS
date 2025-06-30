3a854e8b4e6a681855d7b4007f65fea5a2ffdcc8
Niiixkz <niiixkz@gmail.com>
2025-06-30 17:47:03 +0800

    Change fcitx5 config form home-manager generate to file linking

--- hyprland/fcitx5/default.nix
+++ hyprland/fcitx5/default.nix
@@ -4,6 +4,9 @@
   lib,
   ...
 }:
+let
+  fcitx5Path = "${config.home.homeDirectory}/nixos/hyprland/fcitx5/config";
+in
 {
   i18n.inputMethod = {
     type = "fcitx5";
@@ -15,66 +18,8 @@
         rime-data
         fcitx5-gtk
       ];
-      settings = {
-        globalOptions = {
-          Hotkey = {
-            EnumerateWithTriggerKeys = "True";
-            EnumerateSkipFirst = "False";
-            ModifierOnlyKeyTimeout = 250;
-          };
-
-          "Hotkey/AltTriggerKeys"."0" = "Shift+Shift_R";
-
-          Behavior = {
-            ActiveByDefault = "True";
-            resetStateWhenFocusIn = "No";
-            ShareInputState = "All";
-            PreeditEnabledByDefault = "True";
-            ShowInputMethodInformation = "True";
-            showInputMethodInformationWhenFocusIn = "True";
-            CompactInputMethodInformation = "True";
-            ShowFirstInputMethodInformation = "True";
-            DefaultPageSize = 10;
-            OverrideXkbOption = "False";
-            PreloadInputMethod = "True";
-            AllowInputMethodForPassword = "False";
-            ShowPreeditForPassword = "False";
-            AutoSavePeriod = 30;
-          };
-        };
-
-        inputMethod = {
-          GroupOrder."0" = "Default";
-          "Groups/0" = {
-            Name = "Default";
-            "Default Layout" = "us";
-            DefaultIM = "rime";
-          };
-          "Groups/0/Items/0".Name = "rime";
-        };
-
-        addons = {
-          classicui.globalSection = {
-            "Vertical Candidate List" = "True";
-            WheelForPaging = "True";
-            Font = "Sans 10";
-            MenuFont = "Sans 10";
-            TrayFont = "Sans Bold 10";
-            TrayOutlineColor = "#000000";
-            TrayTextColor = "#ffffff";
-            PreferTextIcon = "False";
-            ShowLayoutNameInIcon = "True";
-            UseInputMethodLanguageToDisplayText = "True";
-            Theme = "default-dark";
-            DarkTheme = "default-dark";
-            UseDarkTheme = "False";
-            UseAccentColor = "True";
-            PerScreenDPI = "False";
-            ForceWaylandDPI = 0;
-            EnableFractionalScale = "True";
-          };
-        };
-      };
     };
   };
+
+  xdg.configFile."fcitx5".source = config.lib.file.mkOutOfStoreSymlink fcitx5Path;
 }
