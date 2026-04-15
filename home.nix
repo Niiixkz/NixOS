3a9706300aaa7e3e69cd10a9f601717dc27bf6ad
Niiixkz <niiixkz@gmail.com>
2026-04-15 18:49:43 +0800

    Remove gtk setting

--- home.nix
+++ home.nix
@@ -10,34 +10,6 @@
   home.username = "niiixkz";
   home.homeDirectory = "/home/niiixkz";
 
-  gtk = {
-    enable = true;
-    theme = {
-      name = "Materia-dark";
-      package = pkgs.materia-theme;
-    };
-    iconTheme = {
-      name = "Dracula";
-      package = pkgs.dracula-icon-theme;
-    };
-    cursorTheme = {
-      name = "miku-cursor";
-      size = 28;
-    };
-
-    gtk2.extraConfig = ''
-      gtk-im-module = "fcitx"
-    '';
-
-    gtk3.extraConfig = {
-      gtk-im-module = "fcitx";
-    };
-
-    gtk4.extraConfig = {
-      gtk-im-module = "fcitx";
-    };
-  };
-
   imports = [
     ./packages
   ];
