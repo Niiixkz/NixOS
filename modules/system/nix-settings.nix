45208565f4961a9fc0c027ca1b55ae4da05e7ccf
Niiixkz <niiixkz@gmail.com>
2026-07-27 09:12:50 +0800

    Add nix-gen-gc

--- modules/system/nix-settings.nix
+++ modules/system/nix-settings.nix
@@ -4,23 +4,32 @@
   packages = [
   ];
 
-  nixosModules = {
-    nix = {
-      settings = {
-        auto-optimise-store = true;
-        experimental-features = [
-          "nix-command"
-          "flakes"
-        ];
+  nixosModules =
+    { config, ... }:
+    {
+      nix = {
+        settings = {
+          auto-optimise-store = true;
+          experimental-features = [
+            "nix-command"
+            "flakes"
+          ];
+        };
+
+        gc = {
+          automatic = true;
+          dates = "daily";
+        };
       };
 
-      gc = {
-        automatic = true;
-        dates = "daily";
-        options = "--delete-older-than 7d";
+      systemd.services.nix-gc.wants = [ "nix-gen-gc.service" ];
+
+      systemd.services.nix-gen-gc = {
+        description = "NixOS Generation Garbage Collector";
+        script = "exec ${config.nix.package.out}/bin/nix-env -vvvv --profile /nix/var/nix/profiles/system --delete-generations +5";
+        serviceConfig.Type = "oneshot";
       };
     };
-  };
 
   homeModules =
     { config, ... }:
