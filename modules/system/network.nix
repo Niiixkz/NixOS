29fea08bda928cd46b49afa043969e5df524cf0f
Niiixkz <niiixkz@gmail.com>
2026-08-02 01:47:17 +0800

    Disable ipv6

--- modules/system/network.nix
+++ modules/system/network.nix
@@ -1,5 +1,8 @@
 {
-  networking.hostName = "NixOS";
-  networking.networkmanager.enable = true;
-  networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking = {
+    hostName = "NixOS";
+    networkmanager.enable = true;
+    proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+    enableIPv6 = false;
+  };
 }
