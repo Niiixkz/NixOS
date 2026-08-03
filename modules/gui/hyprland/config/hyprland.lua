18c473838fc822ad6466820ce077070c6eb5840d
Niiixkz <niiixkz@gmail.com>
2026-08-04 07:15:50 +0800

    Run quickshell with GPU

--- modules/gui/hyprland/config/hyprland.lua
+++ modules/gui/hyprland/config/hyprland.lua
@@ -30,7 +30,7 @@ hl.monitor({
 -------------------
 
 hl.on("hyprland.start", function () 
-  hl.exec_cmd("qs")
+  hl.exec_cmd("__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia __VK_LAYER_NV_optimus=NVIDIA_only qs")
   hl.exec_cmd("fcitx5 -r -d")
 end)
 
