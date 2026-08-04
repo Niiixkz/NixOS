a3e5716b0eb821f5ca65b771803a7d93701c4494
Niiixkz <niiixkz@gmail.com>
2026-08-04 08:42:38 +0800

    Update config.fish

--- modules/cli/fish/config/config.fish
+++ modules/cli/fish/config/config.fish
@@ -6,4 +6,25 @@ end
 
 set -U fish_greeting
 
-alias n="nvim"
+alias h="hx"
+
+function t
+    cd /home/niiixkz/NixOS
+    or begin
+        echo "Failed to change directory"
+        return 1
+    end
+
+    git add .
+
+    git diff --cached --quiet
+    and begin
+        echo "Nothing to commit."
+    end
+    or begin
+        git commit -m a
+        or return 1
+    end
+
+    sudo nixos-rebuild test
+end
