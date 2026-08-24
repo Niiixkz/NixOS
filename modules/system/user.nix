59e1d49a5a3dae23ea3cb0c40d9c102a27f95bd0
Niiixkz <niiixkz@gmail.com>
2026-08-24 08:58:49 +0800

    Add user hashedPassword

--- modules/system/user.nix
+++ modules/system/user.nix
@@ -4,12 +4,20 @@
 }:
 
 {
-  users.users.niiixkz = {
-    isNormalUser = true;
-    extraGroups = [
-      "wheel"
-      "networkmanager"
-    ];
-    shell = pkgs.fish;
+  users.users = {
+    niiixkz = {
+      isNormalUser = true;
+      hashedPassword = "$y$j9T$FYajR1x8omnZB/dhnJk1o/$2kxp.LmE2ZIo5CKsamC8NJPcxusoDtXmybVJ1H8/5T2";
+      shell = pkgs.fish;
+
+      extraGroups = [
+        "wheel"
+        "networkmanager"
+      ];
+    };
+
+    root = {
+      hashedPassword = "!";
+    };
   };
 }
